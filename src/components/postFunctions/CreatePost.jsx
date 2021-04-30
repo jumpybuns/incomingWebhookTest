4a9261ea84d2304f78ad2b08dbb9e5a2ccc0eba8
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-04-30 12:59:12 -0700

    hiding url

--- src/components/postFunctions/CreatePost.jsx
+++ src/components/postFunctions/CreatePost.jsx
@@ -2,11 +2,7 @@ import request from 'superagent';
 
 export async function createPost(newPost) {
   try {
-    await request
-      .post(
-        `https://hooks.slack.com/services/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`
-      )
-      .send(newPost);
+    await request.post(`${process.env.REACT_APP_POST_URL}`).send(newPost);
     return;
   } catch (err) {
     throw err;
